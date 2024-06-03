fab1bef8c24f92e7c6ecb2e7162cb5de5339fbbd
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-06-03 23:09:54 +0700

    add: add images slider of homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+import Image1 from "../../assets/hero/women.png";
+import Image2 from "../../assets/hero/shopping.png";
+import Image3 from "../../assets/hero/sale.png";
+
+const ImageList = [
+  {
+    id: 1,
+    img: Image1,
+    title: "Upto 50% off on all Men's Wear",
+    description:
+      "lorem His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    img: Image2,
+    title: "30% off on all Women's Wear",
+    description:
+      "Who's there lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    img: Image3,
+    title: "70% off on all Products Sale",
+    description:
+      "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
+const Hero = () => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
