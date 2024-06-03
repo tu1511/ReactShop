29a73e1f0fc8d38d3c0d57231a5c59c91faa9fb3
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-06-04 00:11:26 +0700

    style: add slider of homepage

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image1 from "../../assets/hero/women.png";
 import Image2 from "../../assets/hero/shopping.png";
 import Image3 from "../../assets/hero/sale.png";
+import Slider from "react-slick";
 
 const ImageList = [
   {
@@ -30,7 +31,78 @@ const ImageList = [
 ];
 
 const Hero = () => {
-  return <div>Hero</div>;
+  var settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  };
+  return (
+    <div
+      className="relative overflow-hidden min-h-[550px]
+    sm:min-h-[650px] bg-gray-100 flex justify-center
+    items-center dark:bg-gray-950 dark:text-white
+    duration-200"
+    >
+      {/* Background pattern */}
+      <div
+        className="h-[700px] w-[700px] bg-primary/40
+      absolute -top-1/2 right-0 rounded-3xl rotate-45
+      -z-9"
+      ></div>
+      {/* hero section */}
+      <div className="container pb-8 sm:pb-0">
+        <Slider {...settings}>
+          {ImageList.map((data) => (
+            <div key={data.id}>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* text content section */}
+                <div
+                  className="flex flex-col justify-center 
+            gap-4 pt-12 sm:pt-0 text-center
+            sm:text-left order-2 sm:order-1 relative z-10"
+                >
+                  <h1
+                    className="text-5xl sm:text-6xl
+              lg:text-7xl font-bold"
+                  >
+                    {data.title}
+                  </h1>
+                  <p className="text-sm">{data.description}</p>
+                  <div>
+                    <button
+                      className="bg-gradient-to-r from-primary to-secondary hover:scale-105 
+                duration-200 text-white py-2 px-4 rounded-full"
+                    >
+                      Order Now
+                    </button>
+                  </div>
+                </div>
+                {/* image section */}
+                <div className="order-2 sm:order-1">
+                  <div className="relative z-10">
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="w-[300px] h-[300px] sm:h-[450px]
+                sm:w-[450px] sm:scale-125 
+                lg:scale-120 object-contain mx-auto"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
