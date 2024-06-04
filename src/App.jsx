3583623b5d0fe9890da4c8e8f92580c6a83b1973
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-06-04 23:50:12 +0700

    style: add best products

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import TopProducts from "./components/TopProducts/TopProducts";
 
 const App = () => {
   React.useEffect(() => {
@@ -21,6 +22,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Products />
+      <TopProducts />
     </div>
   );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -98,6 +98,12 @@ const Products = () => {
               </div>
             ))}
           </div>
+          {/* view all button */}
+          <div className="flex justify-center">
+            <button className="text-center mt-10 cursor-pointer bg-primary text-white py-1 px-5 rounded-md">
+              View All Button
+            </button>
+          </div>
         </div>
       </div>
     </div>
