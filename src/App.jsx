3cbd351ea7e9116b62ed0ebd74f3d58ed350b180
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-06-04 23:12:30 +0700

    style: add style aos for my web

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
     <div>
       <Navbar />
       <Hero />
+      <Products />
     </div>
   );
 };
